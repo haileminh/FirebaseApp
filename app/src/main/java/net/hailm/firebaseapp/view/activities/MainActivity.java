4b1e1e7fb395876aa6fba90fcbcb3f650443a31a
hai.lm <haileminhit@gmail.com>
2018-04-23 22:51:44 +0700

    fix load progressBar

--- app/src/main/java/net/hailm/firebaseapp/view/activities/MainActivity.java
+++ app/src/main/java/net/hailm/firebaseapp/view/activities/MainActivity.java
@@ -62,4 +62,16 @@ public class MainActivity extends BaseActivity {
             }
         });
     }
+
+    @Override
+    public void onBackPressed() {
+        int count = getFragmentManager().getBackStackEntryCount();
+
+        if (count == 0) {
+            super.onBackPressed();
+            //additional code
+        } else {
+            getFragmentManager().popBackStack();
+        }
+    }
 }
--- app/src/main/java/net/hailm/firebaseapp/view/fragments/HouseFragment.java
+++ app/src/main/java/net/hailm/firebaseapp/view/fragments/HouseFragment.java
@@ -1,24 +1,19 @@
 package net.hailm.firebaseapp.view.fragments;
 
-import android.annotation.TargetApi;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.location.Location;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,18 +30,15 @@ import net.hailm.firebaseapp.listener.HouseListener;
 import net.hailm.firebaseapp.listener.HouseRcvAdapterCallback;
 import net.hailm.firebaseapp.model.dbhelpers.HouseDbHelper;
 import net.hailm.firebaseapp.model.dbmodels.HouseModel;
-import net.hailm.firebaseapp.view.activities.HouseDetailActivity;
 import net.hailm.firebaseapp.view.adapters.HouseRcvAdapter;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 import butterknife.Unbinder;
 
 /**
@@ -201,14 +193,14 @@ public class HouseFragment extends Fragment implements HouseRcvAdapterCallback {
         LogUtils.d("onCLick...." + houseModel.getHouseId());
         LogUtils.d("onCLick 2...." + houseModel.getCommentModelList().size());
         HouseDetailFragment houseDetailFragment = new HouseDetailFragment();
-        manager = getFragmentManager();
+        manager = getActivity().getSupportFragmentManager();
         transaction = manager.beginTransaction();
 
         Bundle bundle = new Bundle();
         bundle.putParcelable(Constants.HOUSE_MODEL, houseModel);
         houseDetailFragment.setArguments(bundle);
 
-        transaction.replace(R.id.frame_container, houseDetailFragment);
+        transaction.replace(android.R.id.content, houseDetailFragment);
         transaction.addToBackStack(null);
         transaction.commit();
 
